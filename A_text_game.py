aa7136b8dbfea36a152b1c4d486a5a0b00f01eac
szal-balazs <szal.balazs@gmail.com>
2023-06-11 20:24:50 +0200

    1

--- A_text_game.py
+++ A_text_game.py
@@ -8,30 +8,70 @@ class Character:
         self.power = power
 
 
+# Formats -----------------------------------------------------------
 def format_ans_lists(ans):
     y = ', '.join(ans)
-    x = f"\033[92m {y} \033[0m"
+    x = f"\033[92m {y}\033[0m"
+    return x
+
+
+def format_stats(ans):
+    x = f"\033[36m{ans}\033[0m"
+    return x
+
+
+def format_enemy_name(ans):
+    x = f"\033[31m{ans}\033[0m"
+    return x
+
+
+def format_items(ans):
+    x = f"\033[92m{ans}\033[0m"
     return x
 
 
 # Player setup ----------------------------------------------------
 class Player(Character):
 
+    def __init__(self, name, hp, power):
+        super().__init__(name, hp, power)
+        self.max_hp = 20
+
     @classmethod
     def attack(cls):
-        enemy.hp = int(enemy.hp) - int(player.power)
-        print(f"You attacked.\n"
-              f"{enemy.name} has {enemy.hp} hp left.")
+        enemy.hp = int(enemy.hp) - int(player.damage())
+        print(f"\nYou attacked.")
+        print(f"You dealt {player.damage()} damage.")
+        if not e_dead():
+            print(f"{format_enemy_name(enemy.name)} has {format_stats(enemy.hp)} hp left.\n")
+        else:
+            print(f"{format_enemy_name(enemy.name)} has no hp left.\n")
+
+    @classmethod
+    def damage(cls):
+        if False:
+            pass
+        else:
+            dmg = int(player.power)
+        return dmg
+
+    fury_cd = 0
+
+    @classmethod
+    def skill_fury(cls):
+        pass
 
     @classmethod
     def name_create(cls):
         player.name = input("What is your name?\n")
-        print(f"Your name is {player.name}.\n")
+        if len(player.name) == 0:
+            player.name = "Anon"
+        print(f"Your name is {format_stats(player.name)}.\n")
 
     @classmethod
     def create(cls):
-        player.hp = 15
-        player.power = 4
+        player.hp = 20
+        player.power = 3
 
     @classmethod
     def stat(cls):
@@ -40,8 +80,13 @@ class Player(Character):
             "HP": player.hp,
             "Power": player.power
         }
-        print(f"Your stats are:")
-        print(f"{stat}\n")
+
+        key_format = "\033[34m"
+        value_format = "\033[36m"
+        key_format_end = "\033[0m"
+        print(f"\nYour stats are:")
+        for key in stat.keys():
+            print(key_format, key, ":", value_format, stat[key], key_format_end)
 
 
 move_list = ["attack", "run"]
@@ -50,11 +95,10 @@ move_list = ["attack", "run"]
 # Enemy setup -----------------------------------------------------
 class Enemy(Character):
 
-    @classmethod
-    def attack(cls):
-        player.hp = int(player.hp) - int(enemy.power)
-        print(f"{enemy.name} is attacked you.\n"
-              f"Your hp is {player.hp}.\n")
+    def __init__(self, name, hp, power):
+        super().__init__(name, hp, power)
+        self.mob_class = ""
+        self.type = ""
 
     @classmethod
     def generate_enemy(cls):
@@ -62,15 +106,97 @@ class Enemy(Character):
         enemy.name = random.choice(enemy_name_list)
         enemy.hp = 10
         enemy.power = 2
+        enemy.mob_class = "mob"
+        enemy.type = enemy.type_def()
+
+    @classmethod
+    def generate_boss(cls):
+        enemy_name_list = ["Dark Troll", "Necromancer", "Mad Tree"]
+        enemy.name = random.choice(enemy_name_list)
+        enemy.hp = 13
+        enemy.power = 3
+        enemy.mob_class = "boss"
+        enemy.type = enemy.type_def()
+
+    # Mob Types
+    unholy = ("Vampire", "Dark Troll")
+    undead = ("Zombie", "Necromancer")
+    creature = ("Werewolf", "Mad Tree")
+
+    @staticmethod
+    def type_def():
+        if enemy.name in Enemy.unholy:
+            return "Unholy"
+        elif enemy.name in Enemy.undead:
+            return "Undead"
+        elif enemy.name in Enemy.creature:
+            return "Creature"
 
     @classmethod
     def stat(cls):
         e_stat = {
             "Name": enemy.name,
             "HP": enemy.hp,
-            "Power": enemy.power
+            "Power": enemy.power,
+            "Type": enemy.type
         }
-        print(f"{e_stat}\n")
+
+        key_format = "\033[34m"
+        value_format = "\033[36m"
+        key_format_end = "\033[0m"
+        for key in e_stat.keys():
+            print(key_format, key, ":", value_format, e_stat[key], key_format_end)
+
+    spec_att_timer = 0
+
+    @classmethod
+    def attack(cls):
+        print(f"{format_enemy_name(enemy.name)} is attacked you.")
+
+        # Special pre damage interactions
+        if enemy.name == "Werewolf":
+            enemy.werewolf_special()
+
+        # Taking damage
+        player.hp = int(player.hp) - int(enemy.damage())
+
+        print(f"{enemy.name} dealt {enemy.damage()} damage to you.")
+
+        # Resetting damage
+
+        if enemy.name == "Werewolf" and Enemy.spec_att_timer <= 3:
+            enemy.spec_att_timer = 0
+
+        # Special after damage interactions
+        if enemy.name == "Vampire":
+            enemy.vampire_special()
+
+        # Summary
+        if not p_dead():
+            print(f"Your hp is {format_stats(player.hp)}.\n")
+        else:
+            print(f"You have no hp left.\n")
+
+    @classmethod
+    def damage(cls):
+        if enemy.name == "Werewolf" and Enemy.spec_att_timer == 3:
+            bonus_dmg = 2
+        else:
+            bonus_dmg = 0
+        dmg = int(enemy.power) + bonus_dmg
+        return dmg
+
+    @classmethod
+    def werewolf_special(cls):
+        Enemy.spec_att_timer += 1
+        if Enemy.spec_att_timer == 3:
+            print(f"The {format_enemy_name(enemy.name)} slashed you with its claws.")
+
+    @classmethod
+    def vampire_special(cls):
+        v_heal = round(random.randint(0, int(enemy.damage()))/2)
+        enemy.hp += v_heal
+        print(f"{enemy.name} healed {format_stats(v_heal)} hp and has {format_stats(enemy.hp)} hp.")
 
 
 # Items ----------------------------------------------------------------
@@ -83,34 +209,100 @@ class Item:
     def get_item(self):
         if "items" not in move_list:
             move_list.append("items")
-        inventory.append(self.name)
-        inventory_dict[self.name] = inventory.count(self.name)
+        if self.name not in inventory_dict:
+            inventory_dict[self.name] = 1
+        else:
+            inventory_dict[self.name] += 1
+
+    @classmethod
+    def use_h_potion(cls):
+        if inventory_dict[health_potion.name] <= 0:
+            print(f"You have no {format_items(health_potion.name)}")
+        else:
+            inventory_dict[health_potion.name] -= 1
+            heal = 5
+            max_heal = player.hp + heal
+            if max_heal > player.max_hp:
+                heal = player.max_hp - player.hp
+            player.hp += heal
+            print(f"You healed {heal} hp. Your hp is {format_stats(player.hp)}.\n")
+
+    @classmethod
+    def use_sword(cls):
+        if "sword" not in equipped:
+            if sword.name not in inventory_dict:
+                print(f"You have no {format_items(sword.name)}")
+            else:
+                inventory_dict[sword.name] -= 1
+                player.power += 2
+                equipped.append("Sword")
+                print(f"You equipped the sword. Your power is {format_stats(player.power)} now.\n")
+        if "sword" in equipped:
+            print(f"You already have a {format_items(sword.name)} equipped.\n")
+
+    @classmethod
+    def use_golden_apple(cls):
+        if inventory_dict[golden_apple.name] <= 0:
+            print(f"You have no {format_items(health_potion.name)}")
+        else:
+            inventory_dict[golden_apple.name] -= 1
+            player.max_hp += 4
+            print(f"You maximum hp increased with 4. Your maximum hp is {format_stats(player.max_hp)}.\n")
 
 
-def use_health_p():
-    heal = 5
-    max_heal = player.hp + heal
-    if max_heal > 15:
-        heal = 15 - player.hp
-    player.hp += heal
-    inventory.remove("Health potion")
-    inventory_dict["Health potion"] = inventory.count("Health potion")
-    print(f"You healed {heal} hp. Your hp is {player.hp}.\n")
+class Bomb(Item):
+
+    @classmethod
+    def generate_bomb(cls):
+
+        bomb_names = ("Holy grenade", "Life bomb", "Fire bomb")
+        bomb.name = random.choice(bomb_names)
+
+    @classmethod
+    def use_bomb(cls):
+
+        inventory_dict[bomb.name] -= 1
+
+        def is_weak():
+            if enemy.type == "Unholy" and bomb.name == "Holy grenade":
+                return True
+            elif enemy.type == "Undead" and bomb.name == "Life bomb":
+                return True
+            elif enemy.type == "Creature" and bomb.name == "Fire bomb":
+                return True
+            else:
+                return False
+
+        if is_weak():
+            enemy.hp -= 6
+            print("You dealt 6 damage.")
+        else:
+            enemy.hp -= 3
+            print("You dealt 3 damage.")
+        if not e_dead():
+            print(f"{format_enemy_name(enemy.name)} has {format_stats(enemy.hp)} hp left.\n")
+        else:
+            print(f"{format_enemy_name(enemy.name)} has no hp left.\n")
 
 
-def use_sword():
-    if "sword" not in equipped:
-        player.power += 2
-        inventory.remove("Sword")
-        inventory_dict["Sword"] = inventory.count("Sword")
-        equipped.append("Sword")
-        print(f"You equipped the sword. Your power is {player.power} now.\n")
-    if "sword" in equipped:
-        print("You already have a sword equipped.\n")
-
-
+# Generate items ----
+bomb = Bomb("")
 health_potion = Item("Health potion")
 sword = Item("Sword")
+golden_apple = Item("Golden apple")
+
+
+def inventory_print():
+    key_format = "\033[92m"
+    key_format_end = "\033[0m"
+    print(f"Your inventory:")
+
+    if sum(inventory_dict.values()) > 0:
+        for key in inventory_dict:
+            if inventory_dict[key] > 0:
+                print(key_format, key, key_format_end, ":", inventory_dict[key])
+    else:
+        print("Empty")
 
 
 # Game components -------------------------------------------------------
@@ -123,73 +315,126 @@ def answer_check(answer_list, question):
 
 
 def fight():
-    print(f"You have been attacked by {enemy.name}.")
+    print(f"You have been attacked by {format_enemy_name(enemy.name)}.")
     enemy.stat()
     skip_list = ["run"]
     fight_question = f"What do you want to do ?{format_ans_lists(move_list)}"
     move_ans = answer_check(move_list, fight_question)
 
+    def fight_death_check():
+        global monster_killed
+        if p_dead():
+            print(f"You died! Monster(s) killed: {monster_killed}\n")
+            return True
+        elif e_dead():
+            print("You win!\n")
+            monster_killed += 1
+            return True
+
     while move_ans not in skip_list:
         while move_ans == "attack":
             global monster_killed
             player.attack()
-            enemy.attack()
-            if player.hp <= 0:
-                print(f"You died! Monster(s) killed: {format_ans_lists(monster_killed)}\n")
+            if not e_dead():
+                enemy.attack()
+            if fight_death_check():
                 skip_list.append("exit")
                 move_ans = "exit"
-            elif enemy.hp <= 0:
-                print("You win!\n")
-                monster_killed += 1
-                move_ans = "exit"
-                skip_list.append("exit")
             else:
                 move_ans = answer_check(move_list, fight_question)
         if "items" in move_list:
             while move_ans == "items":
-                print(f"Your inventory:")
-                print(inventory_dict)
-                item_use_ans = input(f"What item do you want to use (or go\033[92m back\033[0m)?\n")
-                while item_use_ans not in inventory:
+                inventory_print()
+                item_use_ans = input(f"\nWhat item do you want to use (or go\033[92m back\033[0m)?\n")
+                while item_use_ans not in inventory_dict.keys():
                     if item_use_ans == "back":
                         break
                     else:
-                        item_use_ans = str(input(f"I don't understand !\n"
-                                                 f"Your inventory:\n"
-                                                 f"{inventory_dict}\n"
-                                                 f" Try again or go\033[92m back\033[0m):\n"))
+                        inventory_print()
+                        item_use_ans = input(f"Try again or go\033[92m back\033[0m\n")
+
                 if item_use_ans == "Health potion":
-                    use_health_p()
+                    health_potion.use_h_potion()
                 elif item_use_ans == "Sword":
-                    use_sword()
-                move_ans = input(f"What do you want to do ? {move_list}\n")
-                while move_ans not in move_list:
-                    move_ans = str(input(f"I don't understand ! Try again {move_list}:\n"))
+                    sword.use_sword()
+                elif item_use_ans == "Golden apple":
+                    golden_apple.use_golden_apple()
+                elif item_use_ans in ("Holy grenade", "Life bomb", "Fire bomb"):
+                    bomb.name = item_use_ans
+                    bomb.use_bomb()
+
+                if fight_death_check():
+                    skip_list.append("exit")
+                    move_ans = "exit"
+                else:
+                    move_ans = answer_check(move_list, fight_question)
+
     if move_ans == "run":
         print("You ran away!\n")
     if "exit" in skip_list:
         skip_list.remove("exit")
 
 
-def chest():
-    chest_question = f"You found a chest. Do you want to open it ? {format_ans_lists(ans_list)}"
-    chest_ans = answer_check(ans_list, chest_question)
+def looting():
+    lootable_names = ("chest", "pouch")
+    loot_question = f"You found a {random.choice(lootable_names)}.\n" \
+                    f"Do you want to search it ? {format_ans_lists(ans_list)}"
+    loot_ans = answer_check(ans_list, loot_question)
 
-    if chest_ans == "yes":
-        loot_roll = random.randint(1, 100)
-        if loot_roll <= 20:
-            print("There is nothing in there.\n")
-        elif 20 < loot_roll <= 90:
+    def loot_table():
+        # loot common item
+        if 10 <= loot_roll < 41:
             health_potion.get_item()
-            print(f"You found a {health_potion.name}.\n")
-        elif loot_roll >= 90:
+            loot_dict["Health potion"] += 1
+        elif 41 <= loot_roll < 79:
+            bomb.generate_bomb()
+            bomb.get_item()
+            loot_dict[bomb.name] += 1
+        # loot rare item
+        elif 80 <= loot_roll > 99:
             sword.get_item()
-            print(f"You found a {sword.name}.\n")
+            loot_dict["Sword"] += 1
+            # loot legendary item
+        elif 99 < loot_roll:
+            golden_apple.get_item()
+            loot_dict["Golden apple"] += 1
+
+    loot_dict = {
+        "Health potion": 0,
+        "Sword": 0,
+        "Golden apple": 0,
+        "Holy grenade": 0,
+        "Life bomb": 0,
+        "Fire bomb": 0
+    }
+
+    def loot_sum():
+        key_format = "\033[92m"
+        key_format_end = "\033[0m"
+
+        if sum(loot_dict.values()) > 0:
+            print(f"You found:")
+            for key in loot_dict:
+                if loot_dict[key] > 0:
+                    print(loot_dict[key], key_format, key, key_format_end)
+        else:
+            print("There was nothing in there.")
+
+    if loot_ans == "yes":
+        loot_roll = 0
+        if enemy.mob_class == "mob":
+            loot_roll = random.randint(0, 79)
+            loot_table()
+        if enemy.mob_class == "boss":
+            for x in range(2):
+                loot_roll = random.randint(0, 100)
+                loot_table()
+        loot_sum()
 
 
 def restart():
     global run_ans
-    run_question = f"Do you want to restart ? {format_ans_lists(ans_list)}:"
+    run_question = f"Do you want to restart ? {format_ans_lists(ans_list)}"
     run_ans = answer_check(ans_list, run_question)
     if run_ans == "no":
         print("Bye!")
@@ -197,21 +442,28 @@ def restart():
     elif run_ans == "yes":
         global diff_lvl
         diff_lvl = 1
-        inventory.clear()
         inventory_dict.clear()
         player.create()
 
 
-def rounds():
+def mob_round():
     player.stat()
     enemy.generate_enemy()
     fight()
-    if not dead():
-        chest()
+    if not p_dead():
+        looting()
+
+
+def boss_round():
+    player.stat()
+    enemy.generate_boss()
+    fight()
+    if not p_dead():
+        looting()
 
 
 def next_lvl():
-    next_lvl_question = f"Do you want to go to the next level ? {format_ans_lists(ans_list)}:"
+    next_lvl_question = f"Do you want to go to the next level ? {format_ans_lists(ans_list)}"
     next_lvl_ans = answer_check(ans_list, next_lvl_question)
 
     if next_lvl_ans == "no":
@@ -232,30 +484,38 @@ player.create()
 
 diff_lvl = 1
 monster_killed = 0
-inventory = []
 inventory_dict = {}
 equipped = []
 
 
-def dead():
+def p_dead():
     if player.hp > 0:
         return False
     if player.hp <= 0:
         return True
 
 
+def e_dead():
+    if enemy.hp > 0:
+        return False
+    if enemy.hp <= 0:
+        return True
+
+
 while run_ans == "yes":
+
+    print(f"\nThe difficulty level is {diff_lvl}.\n")
 
     enemy = Enemy("", 0, 0)
 
-    print(f"The difficulty level is {diff_lvl}.\n")
-
     for i in range(diff_lvl):
-        if dead():
+        if p_dead():
             break
-        rounds()
+        mob_round()
+    if not p_dead():
+        boss_round()
 
-    if not dead():
+    if not p_dead():
         next_lvl()
-    elif dead:
+    elif p_dead:
         restart()
